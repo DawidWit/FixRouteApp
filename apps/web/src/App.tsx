8751cc1bd79954d889f35c6e245a703b8ac1d844
DawidWit <135219147+DawidWit@users.noreply.github.com>
2025-08-04 15:58:40 +0200

    toast fixes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,5 +3,5 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
-  return <><ToastContainer aria-label={undefined} /><AppRoutes /></>;
+  return <><ToastContainer aria-label={undefined}/><AppRoutes /></>;
 }
--- apps/web/src/utils/toast.tsx
+++ apps/web/src/utils/toast.tsx
@@ -2,13 +2,14 @@ import { toast } from 'react-toastify';
 import "./toast.modules.css";
 
 export const showSuccess = (msg: string) =>
-  toast.success(msg, {
+  toast.success(msg,  {
     position: 'bottom-center',
     autoClose: 1500,
-hideProgressBar: true,
-closeOnClick: true,
-draggable: false,
-className: 'custom-toast-success'
+    hideProgressBar: true,
+    closeOnClick: true,
+    draggable: false,
+    className: 'custom-toast-success',
+    icon: <img src="/icons/success.svg" alt="success" style={{ width: '18px', height: '18px' }} />
   });
 
 export const showError = (msg: string) =>
